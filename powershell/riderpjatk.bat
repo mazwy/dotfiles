1f4c7f23cf752ab2581ee6f6215ac7d59a09f122
mazwy <maczwierzy@gmail.com>
2023-06-01 19:41:21 +0200

    update on riderp

--- powershell/riderpjatk.bat
+++ powershell/riderpjatk.bat
@@ -1,1 +1,1 @@
-runas /netonly /user:pjwstk.edu.pl\s26131 "E:\Jetbrains\apps\Rider\ch-0\223.8617.53\bin\rider64.exe"
+runas /netonly /user:pjwstk.edu.pl\s26131 "E:\Jetbrains\apps\Rider\ch-0\231.8770.54\bin\rider64.exe"
